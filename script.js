606ef3f0ae6722bc6f3a32f98efa4e5dde23159e
Daniela Granda Ahumada <karen.grandah@gmail.com>
2025-03-05 17:47:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("hello")
+
+
+function clearNumber() {
+    document.getElementById("playerNumber").value = "";
+
+  }
+
+let x = Math.floor((Math.random() * 10) + 1); 
+// Math.random numero aleatorio entre 0 y 1 ese numero x 100 me da un nimero entre 0 y 100.99 despues utilizo el math.floor para convertir el numero en entero pero este lleva el nulero hacia abajo
+console.log(x)
+
+
+
+function guessNumber(){
+    let z = document.getElementById("playerNumber").value;
+
+    if (z == x) {
+        alert("You Guess the number")
+        
+    } else if (z < x) {
+        alert("you should look for a larger number");   
+    }else {
+        alert("you should look for a small number");
+    }
+
+}
+
+
+
+
+
+
+
+ 
